b7047a45d1afaea438ae9f5aa355993d2ad585b0
Martin Peeters _martin.peeters@affinitic.be_ <martin.peeters@affinitic.be>
2013-03-28 09:35:25 +0100

    - Initial commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(
+    name='affinitic.testing',
+    version=version,
+    description="",
+    long_description=open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+      "Programming Language :: Python",
+      ],
+    keywords='',
+    author='Affinitic',
+    author_email='info@affinitic.be',
+    url='http://github.com/affinitic/affinitic.testing/',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['affinitic'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    extras_require=dict(
+        test=[
+           'unittest2',
+           'mock',
+         ]),
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    )
