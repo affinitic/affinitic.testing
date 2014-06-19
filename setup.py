043fda7605b039a07c5e3d4550ed77514fba76c8
Martin Peeters <martin.peeters@affinitic.be>
2014-06-19 11:13:26 +0200

    Use rst file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='affinitic.testing',
     version=version,
     description="Test tools",
-    long_description=open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=open(os.path.join("docs", "HISTORY.rst")).read(),
     # Get more strings from
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
