f0fb5fccc97db67bfeb8632043dbce2b70f7a010
Martin Peeters <martin.peeters@affinitic.be>
2014-07-11 16:45:21 +0200

    Fix an error with mysql and multiple sql statements

--- src/affinitic/testing/utils.py
+++ src/affinitic/testing/utils.py
@@ -7,8 +7,16 @@ Licensed under the GPL license, see LICENCE.txt for more details.
 Copyright by Affinitic sprl
 """
 
+import sqlparse
+
 
 def import_data_from_file(session, filepath):
     fd = open(filepath, 'r')
-    session.bind.execute(fd.read())
+    for sql in split_sql_statements(fd.read()):
+        session.bind.execute(sql)
     fd.close()
+
+
+def split_sql_statements(sql):
+    sql = ''.join(sql.splitlines())
+    return sqlparse.split(sql)
